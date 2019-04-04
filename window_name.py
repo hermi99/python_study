8116775e7f7ead02f0636325d2e2f24bc801b3c3
hermi99 <hermi99@daum.net>
2019-04-04 14:53:15 +0900

    Add files via upload

---
+++ window_name.py
@@ -0,0 +1,25 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+uifile = r'.\qt\window_name.ui'
+form, base = uic.loadUiType(uifile)
+
+
+class WindowName(form, base):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.btn_dialog.clicked.connect(self.btn_dialog_clicked)
+
+    def btn_dialog_clicked(self):
+        print("btn_dialog_clicked")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = WindowName()
+    window.show()
+    app.exec_()
